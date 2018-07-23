5e06bda62d767c96e3115ff0e54c18a386c29fa2
janarthan <janarthan@finnovation.lk>
2018-07-23 11:59:25 +0530

    Add Home action

--- session/src/main/java/org/jana/action/HomeAction.java
+++ session/src/main/java/org/jana/action/HomeAction.java
@@ -1,4 +1,19 @@
 package org.jana.action;
 
 public class HomeAction {
+
+    private String username;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String execute() {
+        return "SUCCESS";
+    }
+
 }
